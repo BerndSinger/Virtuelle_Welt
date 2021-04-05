cf3ae1e0785c2f79a898d111ba4c3ea491698390
BerndSinger <45633852+BerndSinger@users.noreply.github.com>
2021-04-05 16:33:25 +0000

    Deploying to gh-pages from master @ 23c96216140b457b75d216dfd9478c7fc5b57b2d 🚀

--- gather_plants_1.2.tsx
+++ gather_plants_1.2.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="gather_plants_1.2" tilewidth="32" tileheight="32" tilecount="880" columns="20">
- <image source="tilesets/Gather_Town/gather_plants_1.2.png" width="640" height="1408"/>
+ <image source="tilesets/gather_plants_1.2.png" width="640" height="1408"/>
  <tile id="84">
   <properties>
    <property name="collides" type="bool" value="true"/>
